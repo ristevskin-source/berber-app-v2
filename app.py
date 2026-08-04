4da59916d115db7a835c667aea56fe7b82bd4f5f
rikiniki <ristevskin@gmail.com>
2026-08-04 11:36:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -670,10 +670,10 @@ def prikaz_nedeljnog_kalendara():
     zaglavlje = st.columns(8)
 
     with zaglavlje[0]:
-        st.write("**Vreme**")
+    st.write("**Vreme**")
 
     for i, d in enumerate(datumi):
-    with zaglavlje[i+1]:
+        with zaglavlje[i+1]:
         st.write(
             f"**INDEX {i}: {d.strftime('%d.%m.%Y')}**"
         )
