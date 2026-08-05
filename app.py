982c3197000baa3116c4eb8d73874adb8bb04af8
rikiniki <ristevskin@gmail.com>
2026-08-05 16:12:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1013,7 +1013,7 @@ with tab2:
                         st.write(telefon)
                     with cols[3]:
                         st.write(f"{usluga} ({cena} din)")
-                    with cols[5]:
+                    with cols[4]:
                         st.write(f"DEBUG status = '{status}'")
                         st.write("STATUS:", repr(status))
 
