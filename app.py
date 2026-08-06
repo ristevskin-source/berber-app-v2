4421963a5046b8627ae0443035df5863eee05b83
rikiniki <ristevskin@gmail.com>
2026-08-06 18:33:43 +0200

    Display status in the app output
    Added status display to the output.

--- app.py
+++ app.py
@@ -1014,6 +1014,7 @@ with tab2:
                     with cols[3]:
                         st.write(f"{usluga} ({cena} din)")
                     with cols[4]:
+                        st.write("STATUS =", repr(status))
                         if status.strip() == 'zakazan':
 
                             if st.button("❌ Otkaži", key=f"otkazi_grupa_{ids[0]}"):
