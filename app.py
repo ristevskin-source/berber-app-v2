640a20b57cf36ed802432c902b5706fa74958a71
rikiniki <ristevskin@gmail.com>
2026-08-09 19:24:08 +0200

    izbacivanje alert

--- app.py
+++ app.py
@@ -1607,9 +1607,7 @@ def prikaz_nedeljnog_kalendara(admin_datum):
     datum,
     vreme
 ) {
-    alert(
-        tip + " | " + datum + " | " + vreme
-    );
+
 
     window.parent.location.search =
         `?akcija=klik&tip=${tip}&datum=${datum}&vreme=${vreme}`;
